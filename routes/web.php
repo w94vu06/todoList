9608f6e2ebabc72955ed1448658ef36fd81bf9a2
Duck <40841131@gm.nfu.edu.tw>
2022-03-31 22:17:41 +0800

    新增頁面

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::any('/test', [TestController::class, 'test']);
 Route::get('/snail',[TestController::class,'snail']);
+Route::get('/',[TestController::class,'snail']);
